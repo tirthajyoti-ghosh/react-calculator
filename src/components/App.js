ebc34fd7dd5074943c34ca93f914ae61ecc6cfdc
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-28 16:29:32 +0530

    Update app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import '../assets/App.css';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display />
+      <ButtonPanel />
     </div>
   );
 }
