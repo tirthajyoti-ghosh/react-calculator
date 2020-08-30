5abead03fcc9bbb8fa90ee7a84859fac9248a691
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-30 12:42:40 +0530

    Add styles for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  const { name } = props;
-  return <button type="button" className="mdl-button mdl-js-button mdl-js-ripple-effect">{name}</button>;
+  const { name, wide, color } = props;
+
+  const style = {
+    width: wide ? '50%' : '',
+    backgroundColor: color ? '' : 'orange'
+  };
+
+  return <button type="button" className="mdl-button mdl-js-button mdl-js-ripple-effect" style={ style }>{name}</button>;
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  wide: PropTypes.bool
+};
+
+Button.defaultProps = {
+  wide: false
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,32 +5,32 @@ function ButtonPanel() {
   return (
     <div className="button-panel">
       <div className="row">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button name="AC" color />
+        <Button name="+/-" color />
+        <Button name="%" color />
         <Button name="÷" />
       </div>
       <div className="row">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color/>
+        <Button name="8" color />
+        <Button name="9" color />
         <Button name="×" />
       </div>
       <div className="row">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color />
+        <Button name="5" color />
+        <Button name="6" color />
         <Button name="-" />
       </div>
       <div className="row">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+        <Button name="1" color />
+        <Button name="2" color />
+        <Button name="3" color />
         <Button name="+" />
       </div>
       <div className="row">
-        <Button name="0" />
-        <Button name="." />
+        <Button name="0" color wide />
+        <Button name="." color />
         <Button name="=" />
       </div>
     </div>
