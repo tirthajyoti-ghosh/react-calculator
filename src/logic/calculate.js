21fa6b4b50439d80731dd1f2e214f4d2d55f5220
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-01 20:24:53 +0530

    Import operate in calculate

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,14 @@
+import operate from "./operate";
+
 export default function calculate(data={ total: '', next: '', operation: ''}, buttonName) {
   let { total, next } = data;
 
   if (buttonName === '+/-') {
     total *= -1;
     next *= -1;
+
+    return next;
   }
+
+  return operate(total, next, buttonName);
 }
