b0c6617891a2d750852399319115c243f95a86c4
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-02 17:36:10 +0530

    Convert stateless App to stateful

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,25 @@ import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App mdl-card mdl-shadow--2dp">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App mdl-card mdl-shadow--2dp">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
