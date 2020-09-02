a35cdec5bdcaff1d6f4ef482326624568b754585
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-03 00:18:06 +0530

    Set up on click events

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,12 +4,23 @@ import PropTypes from 'prop-types';
 function Button(props) {
   const { name, wide, color } = props;
 
+  function handleClick(buttonName) {
+    return props.clickHandler(buttonName);
+  }
+
   const style = {
     width: wide ? '50%' : '', // width increased to 50% just to make orange buttons line up properly
     backgroundColor: color ? '' : 'orange',
   };
 
-  return <button type="button" className="mdl-button mdl-js-button mdl-js-ripple-effect" style={style}>{name}</button>;
+  return <button 
+          type="button" 
+          className="mdl-button mdl-js-button mdl-js-ripple-effect" 
+          onClick={ () => { handleClick(name); }}
+          style={style}
+          >
+            {name}
+          </button>;
 }
 
 Button.propTypes = {
